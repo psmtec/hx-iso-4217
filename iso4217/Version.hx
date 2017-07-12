0f5bc822f4d69b08441a7c87d2eeccb3d4b924ba
David Klein <david.klein@psmtec.de>
2017-07-12 17:20:56 +0200

    update version as well

--- iso4217/Version.hx
+++ iso4217/Version.hx
@@ -2,6 +2,6 @@ package iso4217;
 
 class Version {
 	public static var year = 2017;
-	public static var month = 1;
-	public static var day = 1;
+	public static var month = 6;
+	public static var day = 9;
 }
