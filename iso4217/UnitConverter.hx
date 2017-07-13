cb6ea2494f2e4136d7a72d85f032af27b3291e69
David Klein <david.klein@psmtec.de>
2017-07-13 11:03:23 +0200

    rename CurrencyTools to UnitConverter

--- iso4217/UnitConverter.hx
+++ iso4217/UnitConverter.hx
@@ -1,7 +1,7 @@
 package iso4217;
 
 @:expose @:keep
-class CurrencyTools {
+class UnitConverter {
 	public static function minorToMajor( cc: CurrencyCode, value: Int ) : Float {
 		var currency: CurrencyData = Reflect.field(CurrencyDetails, cc);
 		return value * currency.minorToMajor;
