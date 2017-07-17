1e7b7a5b0aee7cd905d0b55c5029c39d2de75cde
David Klein <david.klein@psmtec.de>
2017-07-17 10:11:51 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// const dist = require('./dist/index.js').iso4217;
-
-// console.log(dist);
-
-// module.exports = {
-// 	version: {
-// 		year: dist.Version.year,
-// 		month: dist.Version.month,
-// 		day: dist.Version.day,
-// 	},
-// }
-
 module.exports = require('./dist/index.js').iso4217;
